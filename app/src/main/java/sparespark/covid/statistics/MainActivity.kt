7e9939b4818434940526ea225ffa584e23939af2
Mohamed <thisismohalawa@gmail.com>
2022-11-02 14:43:09 +0200

    Master Empty Project.

---
+++ app/src/main/java/sparespark/covid/statistics/MainActivity.kt
@@ -0,0 +1,16 @@
+package sparespark.covid.statistics
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+
+        }
+    }
+}
+
---
+++ app/src/main/java/sparespark/covid/statistics/MainApplication.kt
@@ -0,0 +1,7 @@
+package sparespark.covid.statistics
+
+import android.app.Application
+
+class MainApplication : Application() {
+
+}
